0ccc0d84098c8b585281210530dba9504a9fd17f
Justin Hagerty <wheelsmanx@gmail.com>
2020-12-01 09:29:59 -0500

    golf

--- day1/part1.py
+++ day1/part1.py
@@ -1,6 +1,4 @@
 with open('./puzzle','r') as fh:
      all_lines = [int(i.strip()) for i in fh.readlines()]
-for x in all_lines:
-    for y in all_lines:
-        if(x+y == 2020):
-            print(x*y)
+print([x * y for x in all_lines for y in all_lines if(x+y == 2020)][0])
+
--- day1/part2.py
+++ day1/part2.py
@@ -1,7 +1,3 @@
 with open('./puzzle', 'r') as fh:
     all_lines = [int(i.strip()) for i in fh.readlines()]
-for x in all_lines:
-    for y in all_lines:
-        for z in all_lines:
-            if(x + y + z == 2020):
-                print(x * y * z)
+print([x * y * z for x in all_lines for y in all_lines for z in all_lines if(x+y+z == 2020)][0])
